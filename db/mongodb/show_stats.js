a4af08cdda1473fd44deb53561f777e50a2f7d4e
stanislaw_jakiel <stanislaw.dev@gmail.com>
2018-06-26 15:50:15 +0200

    db stat function

--- db/mongodb/show_stats.js
+++ db/mongodb/show_stats.js
@@ -2,5 +2,5 @@ db = db.getSiblingDB('admin');
 dbs = db.runCommand({ "listDatabases": 1 }).databases;
 dbs.forEach(function(database) {
     //in kb
-    db.getSiblingDB(database.name).stats(1024);
+    printjson(db.getSiblingDB(database.name).stats(1024));
 });
