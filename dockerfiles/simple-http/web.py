2a2fc6717e8f4d743161da9adc9a4fccac904d15
stanislaw_jakiel <stanislaw.dev@gmail.com>
2021-01-26 19:00:56 +0100

    very simple web app for testing purposes

---
+++ dockerfiles/simple-http/web.py
@@ -0,0 +1,46 @@
+import logging
+import flask
+import time
+import signal
+import sys
+from flask import Flask
+from flask_api import status
+from os import environ
+
+log = logging.getLogger()
+log.addHandler(logging.StreamHandler())
+log.setLevel(logging.INFO)
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return f"Hello World!: {flask.request.remote_addr}"
+
+@app.route('/health')
+def health():
+    log.info(f"health check from: {flask.request.remote_addr}")
+    return "OK"
+
+@app.route('/sick')
+def sick():
+    return "BAD", status.HTTP_404_NOT_FOUND
+
+@app.route('/broken')
+def broken():
+    time.sleep(42)
+    return "broken", status.HTTP_404_NOT_FOUND
+
+def sigterm_handler(signum, frame):
+    log.info("SIGTERM received, stopping")
+    sys.exit()
+
+if __name__ == '__main__':
+    if environ.get('FLASK_SETTINGS') is not None:
+        log.info("Found flask config")
+        app.config.from_envvar('FLASK_SETTINGS')
+        log.info(f"config: {app.config}")
+
+    signal.signal(signal.SIGTERM, sigterm_handler)
+    app.run(host="0.0.0.0")
